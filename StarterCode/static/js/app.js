30492b40f62eaa207381e49a2fb3f5d47139b668
JGarvert <julie.garvert@adidas.com>
2021-03-30 15:34:57 -0700

    starting on script

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,9 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Use D3 to select object in the HTML code.
+
+var output = d3.select("tbody");
+var filter_button = d3.select("#filter-btn");
+var date_crit = d3.select("#datetime");
+
