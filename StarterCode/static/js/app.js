9a509f7236b08061d2175ca98afe268585d986e2
JGarvert <julie.garvert@adidas.com>
2021-03-30 17:59:06 -0700

    added input data script

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,3 +9,16 @@ var date_crit = d3.select("#datetime");
 
 // Retrieve JSON keys
 var key_list = Object.keys(tableData[0])
+
+// Fill HTML table with data
+var input_data = (data_source ) => {
+    data_source.forEach(sighting => {
+        var next_row = output_table.append("tr");
+        key_list.forEach(key => {
+            next_row.append("td").text(sighting[key]);
+        });
+
+    });
+
+}
+input_data(tableData);
